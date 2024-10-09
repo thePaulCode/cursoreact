e41e5003c5b83e3868771751e0a290804cd12461
Paulo Santos <santospaulor21@gmail.com>
2024-10-09 20:47:55 -0300

    Condicionais e css

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,28 @@
 import React, { FormEvent, useState, useEffect } from "react";
+import './assets/css/app.css';
 
 function App() {
   const usuarios = [{name: 'Paul', lastName: 'Snts'}, 
                     {name: "Ana", lastName: 'Andr'}];
 
+  const [logged, setLogged] = useState(false);
+
   return (
     <>
-      {usuarios
-        .filter(user => user.name.toLowerCase === "PAUL".toLowerCase)
-        .map((usuario) => {
-          return (<>{usuario.name} {usuario.lastName} <br /></>);
-      })}
+      <h1 className="titulo" >SUPERMERCADO STELLA</h1>
+      <h2>Onde a economia é certa!</h2>
+      <br />
+      <br />
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil necessitatibus nisi excepturi culpa porro ipsum cum doloremque! Tempore, alias incidunt in excepturi, molestiae illo nisi, earum exercitationem dolores quos voluptatum!</p>
+      {
+        logged === true && <p>Usuário logado. Promoções do dia.</p> 
+      }
+      {
+        logged === false &&
+        <p>Faça seu Login para ver as Promoções do dia.</p>
+      }
+      <button onClick={()=>{setLogged(true)}}>Login</button>
+      <button onClick={()=>{setLogged(false)}}>Logoff</button>
     </>
   );
 }
