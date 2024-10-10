9cb35bb29bd940709d114ea7aa2d0f168519e813
Paulo Santos <santospaulor21@gmail.com>
2024-10-10 10:53:09 -0300

    Aplicado styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,67 +1,16 @@
 import React, { FormEvent, useState } from "react";
+import {Estado, Botao} from './AppStyled';
 
 function App() {
-  const [primeiroNumero, setPrimeiroNumero] = useState<number>(0);
-  const [segundoNumero, setSegundoNumero] = useState<number>(0);
-  const [operacao, setOperacao] = useState("");
-  const [resultado, setResultado] = useState<number>(0);
 
-  function handleSubmit(e: React.FormEvent) {
-    e.preventDefault();
-    switch (operacao) {
-      case "+":
-        setResultado(primeiroNumero + segundoNumero);
-        break;
-      case "-":
-        setResultado(primeiroNumero - segundoNumero);
-        break;
-      case "*":
-        setResultado(primeiroNumero * segundoNumero);
-        break;
-      case "/":
-        setResultado(primeiroNumero / segundoNumero);
-        break;
-
-      default:
-        break;
-    }
-  }
-
+  const [black, setBlack] = useState(false);
   return (
     <>
-      <form onSubmit={handleSubmit} action="">
-        <input
-          type="number"
-          onChange={(e) => {
-            setPrimeiroNumero(e.target.valueAsNumber);
-          }}
-          value={primeiroNumero}
-        />
-        <select
-          name=""
-          id=""
-          onChange={(e) => {
-            setOperacao(e.target.value);
-          }}
-          value={operacao}
-        >
-          <option value="">--Selecione a operação--</option>
-          <option value="+">+</option>
-          <option value="-">-</option>
-          <option value="/">/</option>
-          <option value="*">*</option>
-        </select>
-        <input
-          type="number"
-          name=""
-          id=""
-          onChange={(e) => {
-            setSegundoNumero(e.target.valueAsNumber);
-          }}
-          value={segundoNumero}
-        />
-        <button type="submit">Calcular</button> <b>Resultado: {resultado}</b>
-      </form>
+      <Estado black={black}>
+        Grêmio
+      </Estado>
+      <Botao onClick={()=>setBlack(!black)}>DARK</ Botao>
+
     </>
   );
 }
